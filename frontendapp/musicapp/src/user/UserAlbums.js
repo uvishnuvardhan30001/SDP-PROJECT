ebbe73e4afbf01684de89ca58a9623f6fd466e1d
Vishnu Vardhan <vishnuvardhan.uppuluri@gmail.com>
2024-03-10 05:22:37 +0530

    images and front end

--- frontendapp/musicapp/src/user/UserAlbums.js
+++ frontendapp/musicapp/src/user/UserAlbums.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import './UserHome.css'
+import logo1 from '../images/thestootage.jpg'
+import logo2 from '../images/melodrama.jpeg'
+import logo3 from '../images/Thriller.jpg'
+import logo4 from '../images/Rakshak.jpg'
+
 
 export default function UserAlbums() {
   return (
-    <div>UserAlbums</div>
+    <div className="main_content">
+      
+    <div className="info">
+      
+
+    <h1 align="center">Feel The Taste Of Music</h1>
+    <br/>
+    <table>
+    
+      <td><div className="card">
+                <img src={logo1} alt='imaf'/>
+                <h3>The Stooges</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo2 }alt='imafr'/>
+                <h3>Melodrama</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo3}alt='imafr'/>
+                <h3>Thriller</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo4}alt='imafr'/>
+                <h3>Rakshak</h3>
+                <button>Play</button>
+            </div></td>
+            
+    </table>
+            
+          
+      
+  </div>
+  
+</div>
   )
 }
--- frontendapp/musicapp/src/user/UserPlayList.js
+++ frontendapp/musicapp/src/user/UserPlayList.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import './UserHome.css'
+import logo1 from '../images/mass.jpeg'
+import logo2 from '../images/love.jpeg'
+import logo3 from '../images/travell.jpeg'
+import logo4 from '../images/melody.jpeg'
 
 export default function UserPlayList() {
   return (
-    <div>UserPlayList</div>
+    <div className="main_content">
+      
+    <div className="info">
+      
+
+    <h1 align="center">Feel The Taste Of Music</h1>
+    <br/>
+    <table>
+    
+      <td><div className="card">
+                <img src={logo1} alt='imaf'/>
+                <h3>Telugu Mass Beats</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo2 }alt='imafr'/>
+                <h3>Love Tunes</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo3}alt='imafr'/>
+                <h3>Travelling</h3>
+                <button>Play</button>
+            </div></td>
+            <td><div className="card">
+                <img src={logo4}alt='imafr'/>
+                <h3>Rakshak</h3>
+                <button>Play</button>
+            </div></td>
+            
+    </table>
+            
+          
+      
+  </div>
+  
+</div>
   )
 }
