7f127c70b90c8828c98b9062c913d8c6ac4ab123
Vishnu Vardhan <vishnuvardhan.uppuluri@gmail.com>
2024-03-09 11:44:48 +0530

    User

--- frontendapp/musicapp/src/App.js
+++ frontendapp/musicapp/src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
 import { BrowserRouter } from "react-router-dom";
-import MainNavBar from "./main/MainNavBar";
-// import UserNavBar from "./user/components/UserNavBar";
+import UserNavBar from "./user/UserNavBar";
+// import MainNavBar from "./main/MainNavBar";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <MainNavBar/>
-      {/* <UserNavBar/> */}
+      {/* <MainNavBar/> */}
+      <UserNavBar/>
       </BrowserRouter>
     </div>
   );
--- frontendapp/musicapp/src/user/UserNavBar.js
+++ frontendapp/musicapp/src/user/UserNavBar.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Routes,Route,Link} from 'react-router-dom'
+import logo from '../images/logo.jpg'
+import UserHome from './UserHome'
+import UserPlayList from './UserPlayList'
+import UserAlbums from './UserAlbums'
+import Artists from './Artists'
+import ProfileCard from './ProfileCard'
+import './UserNavBar.css'
 
 export default function UserNavBar() {
   return (
-    <div>
-      
+    <div className="wrapper">
+    <div className="sidebar">
+        <img className='row' src={logo} alt='logo' />
+        <br/> <br/> <br/>
+    <ul className='navbar'>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/userplaylist">Playlists</Link></li>
+    <li><Link to="/useralbums">Albums</Link></li>
+    <li><Link to="/artists">Artists</Link></li>
+    <li><Link to="/profilecard">Profile</Link></li>
+    </ul>
+    </div>
+    <Routes>
+        <Route path="/" Component={UserHome}/>
+        <Route path="/userplaylist" Component={UserPlayList}/>
+        <Route path="/useralbums" Component={UserAlbums}/>
+        <Route path="/artists" Component={Artists}/>
+        <Route path="/profilecard" element={<ProfileCard/>}/>
+    </Routes>
+
     </div>
   )
 }
