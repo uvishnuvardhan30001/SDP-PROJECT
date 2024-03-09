5347f7bbe77aacfa731469c0027944d94eab5c85
Vishnu Vardhan <vishnuvardhan.uppuluri@gmail.com>
2024-03-09 11:56:54 +0530

    init

--- frontendapp/musicapp/src/App.js
+++ frontendapp/musicapp/src/App.js
@@ -1,16 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
 import { BrowserRouter } from "react-router-dom";
-import UserNavBar from "./user/UserNavBar";
-// import MainNavBar from "./main/MainNavBar";
+// import UserNavBar from "./user/UserNavBar";
+import MainNavBar from "./main/MainNavBar";
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      {/* <MainNavBar/> */}
-      <UserNavBar/>
+      <MainNavBar/>
+      {/* <UserNavBar/> */}
       </BrowserRouter>
     </div>
   );
